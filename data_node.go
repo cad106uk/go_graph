9db14ee9f83ac5d0004900b424d1a80b9f58b75f
Ciaran Doherty <cad106uk@gmail.com>
2013-02-22 13:40:33 +0000

    Moving type nodes into their own file

--- data_node.go
+++ data_node.go
@@ -12,13 +12,6 @@ func (ne *NodeError) Error() string {
 	return ne.msg
 }
 
-type nodeType struct {
-	name        string
-	description string
-}
-
-var allNodeTypes = make(map[string]nodeType)
-
 type dataNode struct {
 	dataType nodeType
 	document           // The data stored at this node
@@ -36,36 +29,4 @@ func (dn *dataNode) Init(dt string, doc document) error {
 		dn.document = doc
 	})
 	return nil
-}
-
-func CreateNewNodeType(name, desc string) error {
-	_, present := allNodeTypes[name]
-	if present {
-		return error(&NodeError{"A NodeType with this name has already been created"})
-	}
-
-	allNodeTypes[name] = nodeType{name, desc}
-	return nil
-}
-
-func GetNodeType(name string) (nodeType, error) {
-	val, present := allNodeTypes[name]
-	if !present {
-		return nodeType{}, error(&NodeError{"No NodeType with this name exists"})
-	}
-	return val, nil
-}
-
-func GetOrCreateNodeType(name, desc string) (nodeType, error) {
-	val, err := GetNodeType(name)
-	if err == nil {
-		return val, nil
-	}
-
-	err = CreateNewNodeType(name, desc)
-	if err == nil {
-		return GetNodeType(name)
-	}
-
-	return nodeType{}, err
 }
---
+++ type_node.go
@@ -0,0 +1,40 @@
+package go_graph
+
+type nodeType struct {
+	name        string
+	description string
+}
+
+var allNodeTypes = make(map[string]nodeType)
+
+func CreateNewNodeType(name, desc string) error {
+	_, present := allNodeTypes[name]
+	if present {
+		return error(&NodeError{"A NodeType with this name has already been created"})
+	}
+
+	allNodeTypes[name] = nodeType{name, desc}
+	return nil
+}
+
+func GetNodeType(name string) (nodeType, error) {
+	val, present := allNodeTypes[name]
+	if !present {
+		return nodeType{}, error(&NodeError{"No NodeType with this name exists"})
+	}
+	return val, nil
+}
+
+func GetOrCreateNodeType(name, desc string) (nodeType, error) {
+	val, err := GetNodeType(name)
+	if err == nil {
+		return val, nil
+	}
+
+	err = CreateNewNodeType(name, desc)
+	if err == nil {
+		return GetNodeType(name)
+	}
+
+	return nodeType{}, err
+}
