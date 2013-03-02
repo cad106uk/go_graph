fc80a07b67a450265930fff1a4bf405eefddbef3
Ciaran Doherty <cad106uk@gmail.com>
2013-03-02 19:06:00 +0000

    Tests for edgetype methods

--- graph_edge_test.go
+++ graph_edge_test.go
@@ -16,3 +16,30 @@ func TestMatchEdgeType(t *testing.T) {
 		t.Error("Found invalid string!")
 	}
 }
+
+func TestEdgeType(t *testing.T) {
+	et := edgeType{
+		"Your Moma", []string{"FailFrom1", "FailFrom2"},
+		[]string{"FailTo1", "FailTo2"}}
+
+	nt, _ := GetOrCreateNodeType("Your", "Moma")
+	dn, _ := CreateDataNode(nt, []byte("Your Moma"))
+	if et.ValidToNode(dn) {
+		t.Error("Failed this data and edge node don't match")
+	}
+	if et.ValidFromNode(dn) {
+		t.Error("Failed this data and edge node don't match")
+	}
+
+	et = edgeType{
+		"Your Moma", []string{"Correct Edge"},
+		[]string{"Correct Edge"}}
+	nt, _ = GetOrCreateNodeType("Correct Edge", "Moma")
+	dn, _ = CreateDataNode(nt, []byte("Your Moma"))
+	if !et.ValidToNode(dn) {
+		t.Error("Failed this data and edge node do match")
+	}
+	if !et.ValidFromNode(dn) {
+		t.Error("Failed this data and edge node do match")
+	}
+}
